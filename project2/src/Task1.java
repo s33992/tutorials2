23f1db1b8f6da35c736ff22b1f0f9fc8dc552440
Aleksander Młynarczyk <s33992@pjwstk.edu.pl>
2025-03-13 13:32:37 +0100

    Task1

--- project2/src/Task1.java
+++ project2/src/Task1.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Podaj miejsce zamieszkania: ");
+        String miejsce = sc.nextLine();
+        System.out.println("Podaj wiek: ");
+        int wiek = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Podaj dzień tygodnia: ");
+        String dzien = sc.nextLine();
+
+        double a;
+        a = 40;
+        double b;
+        b = 0;
+
+
+        if (dzien.equals("Czwartek")) {
+            a = 0;
+            b = 100;
+        } else {
+            if (wiek <= 10) {
+                a = 0;
+                b = 100;
+            } else if (wiek <= 18) {
+                a = a * 0.5;
+                b = b + 50;
+                if (miejsce.equals("Warszawa")) {
+                    a = 0.9 * a;
+                    b = b + 10;
+                }
+            } else {
+                a = a;
+                b = b;
+                if (miejsce.equals("Warszawa")) {
+                    a = 0.9 * a;
+                    b = b + 10;
+                }
+            }
+        }
+
+
+
+
+
+        System.out.println("Dane: " + miejsce + ", " + wiek + " lat, " + dzien + " Cena biletu: " + a + " PLN");
+        System.out.println("Przecena: " + b + "%");
+
+    }
 }
--- project2/src/task2.java
+++ project2/src/task2.java
@@ -1,2 +1,3 @@
 public class task2 {
+
 }
